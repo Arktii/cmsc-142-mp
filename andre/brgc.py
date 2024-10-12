ee8bea8bf7c6e10a99008589a8aaf67980679530
embryzon <95128949+embryzon@users.noreply.github.com>
2024-10-12 15:25:10 +0800

    update gitignore add andre/**/*.py

---
+++ andre/brgc.py
@@ -0,0 +1,14 @@
+# TODO: use bitarray
+
+def brgc(n):
+    if n == 1:
+        return ['0','1']
+    else:
+        l1 = brgc(n-1)
+        l2 = [i for i in l1[::-1]]
+
+        for i in range(len(l1)):
+            l1[i] = '0' + l1[i]
+            l2[i] = '1' + l2[i]
+
+        return l1 + l2
