c0def88a5a2ea4a71467f7afdd0a84aede68f993
Arktii <72131971+Arktii@users.noreply.github.com>
2024-10-12 10:12:06 +0800

    Generate list of items

---
+++ generate_list.py
@@ -0,0 +1,23 @@
+import random
+
+# Constants
+FILE_NAME = "items.py"
+
+ITEM_COUNT = 50
+WEIGHT_RANGE = range(50, 101)
+VALUES_RANGE = range(100, 501)
+
+f = open(FILE_NAME, "w")
+
+# Generate list
+items = []
+for _ in range(ITEM_COUNT):
+    items.append((random.choice(WEIGHT_RANGE), random.choice(VALUES_RANGE)))
+
+# Write to file
+f.write("items = [\n")
+for item in items:
+    f.write("\t(" + str(item[0]) + ", " + str(item[1]) + "),\n")
+f.write("]")
+
+f.close()
