e3db8a03d59f0b4a5231ed9d39175cf4eb5856cf
Arktii <72131971+Arktii@users.noreply.github.com>
2024-10-12 11:15:35 +0800

    Add knapsack naive approach

--- mp1.py
+++ mp1.py
@@ -9,14 +9,23 @@ def main():
     total_start = time.time()
 
     gen_start = time.time()
-    all_subsets = binary_reflected_gray_code(26)
+    all_subsets = binary_reflected_gray_code(21)
     gen_end = time.time()
 
+    search_start = time.time()
+    (solution, solution_weight, solution_value) = knapsack(items, all_subsets)
+    search_end = time.time()
 
+    total_end = time.time()
 
-    print(all_subsets[1:10])
+    print(f"Solution: {solution}")
+    print(f"Weight: {solution_weight}")
+    print(f"Value: {solution_value}")
 
-    print(f"Time: {gen_end - gen_start}")
+    print(f"Total time: {total_end - total_start}")
+    print(f"Generation Time: {gen_end - gen_start}")
+    print(f"Search Time: {search_end - search_start}")
+
 
 
 def binary_reflected_gray_code(n: int) -> list[bitarray]:
@@ -35,6 +44,39 @@ def binary_reflected_gray_code(n: int) -> list[bitarray]:
             
         return list1 + list2
 
+# Starting with naive approach
+def knapsack(items: list[tuple[int, int]], all_subsets = list[bitarray]) -> tuple[bitarray, int, int]:
+    solution = all_subsets[0]
+    solution_weight = 0
+    solution_value = 0
+
+    for candidate in all_subsets:
+        weight = calculate_weight(items, candidate)
+        if weight > KNAPSACK_CAPACITY:
+            continue
+
+        value = calculate_value(items, candidate)
+        if value >= solution_value :
+            solution = candidate
+            solution_weight = weight
+            solution_value = value
+
+    return (solution, solution_weight, solution_value)
+
+
+def calculate_weight(items: list[tuple[int, int]], item_set: bitarray) -> int:
+    weight = 0
+    for i in range(len(item_set)):
+        if item_set[i] == 1:
+            weight += items[i][0]
+    return weight
+
+def calculate_value(items: list[tuple[int, int]], item_set: bitarray) -> int:
+    value = 0
+    for i in range(len(item_set)):
+        if item_set[i] == 1:
+            value += items[i][1]
+    return value
 
 main()
     
