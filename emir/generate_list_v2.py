4aeea56a7261f26adaf30829b17d5578cd8f77b0
Arktii <72131971+Arktii@users.noreply.github.com>
2024-12-17 15:49:38 +0800

    Bugfix not generating 3 unique item sets

--- emir/generate_list_v2.py
+++ emir/generate_list_v2.py
@@ -7,11 +7,6 @@ ITEM_COUNT = 100_000
 WEIGHT_RANGE = range(100, 1501)
 VALUES_RANGE = range(100, 501)
 
-# Generate list
-items = []
-for _ in range(ITEM_COUNT):
-    items.append((random.choice(WEIGHT_RANGE), random.choice(VALUES_RANGE)))
-
 # Write to file
 f = open(FILE_NAME, "w")
 
@@ -21,6 +16,11 @@ f.write("use crate::item::Item;\n")
 
 f.write("pub const ITEM_SETS: [[Item; " + str(ITEM_COUNT) + "]; 3] = [\n")
 for i in range(3):
+    # Generate List
+    items = []
+    for _ in range(ITEM_COUNT):
+        items.append((random.choice(WEIGHT_RANGE), random.choice(VALUES_RANGE)))
+
     f.write("\t[\n")
     for item in items:
         f.write("\t\tItem {\n")
--- rust/mp2/src/item.rs
+++ rust/mp2/src/item.rs
@@ -3,3 +3,5 @@ pub struct Item {
     pub weight: i32,
     pub value: i32,
 }
+
+// TODO? Refactor this to use usize? 
